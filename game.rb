74d37cdb1fcd1fbf884cb784946e52093d8446bf
ChrisPytel <chris_pytel@hotmail.com>
2024-07-05 23:28:34 -0400

    update while to correctly render questions and advance turns

--- game.rb
+++ game.rb
@@ -1,22 +1,36 @@
 require './player'
 require './turn-tracker'
-require './question-generator'
+require './question'
+require './display'
 puts '... loaded game logic'
 
 class Game
   def initialize(p1_name, p2_name)
     @p1_name = p1_name
     @p2_name = p2_name
-    puts "Welcome #{@p1_name} and #{@p2_name} for playing today!"
-    puts "Each player starts game with 3 hearts 💖💖💖"
-    puts "Don't end up with zero hp or you lose 🖤🖤🖤"
+    puts "\nWelcome #{@p1_name} and #{@p2_name} to the 1v1 Math challenge!"
+    puts "\n - Each player starts game with 3 hearts 💖💖💖"
+    puts " - Don't end up with zero HP or you lose 🖤🖤🖤"
+    puts " - Get as many stars as you can for correct answers 🌟"
   end
 
   def play
-    puts "starting game for rl now"
-    player1 = Player.new(@p1_name, 3)
-    player2 = Player.new(@p2_name, 3)
-    round = Turn.new(player1)
-  end
+    player1 = Player.new(@p1_name, 3, 0)
+    player2 = Player.new(@p2_name, 3, 0)
+    turn = Turn.new(player1)
+    display = Display.new(player1, player2)
 
+    #Game ends when either reaches 0 HP
+    while player1.hitpoints != 0 && player2.hitpoints != 0 
+      
+      question = Question.new(turn.current_player)
+      question.generator(turn.current_player)
+
+      display.remaining_HP()
+      display.current_score()
+      turn.alternate(player1, player2)
+    end
+
+    display.end_message()
+  end
 end
