7ebd8e89324f2df7ae95b5e08cd620beed9acb34
ChrisPytel <chris_pytel@hotmail.com>
2024-07-05 23:28:05 -0400

    update player.rb to include score as a value to save

--- player.rb
+++ player.rb
@@ -1,14 +1,15 @@
 # Defines the parameters for a Player class template
-# Will keep track of the name and how much HP the player has
+# Will keep track of the name, how much HP the player has, and their number of correct answers
 
 puts '... loaded player creator'
 
 class Player
-  def initialize (name, hitpoints)
+  def initialize (name, hitpoints, score)
     @name = name
     @hitpoints = hitpoints
+    @score = score
   end
 
-  attr_accessor :name, :hitpoints
+  attr_accessor :name, :hitpoints, :score
 
 end
